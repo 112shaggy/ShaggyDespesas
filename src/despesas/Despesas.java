d242fcf7a886c1157ee6f44db5ce48c9b491dcf9
jtsrodrigues <jtsrodrigues@live.com>
2013-03-14 16:12:45 +0000

    Mudado

--- src/despesas/Despesas.java
+++ src/despesas/Despesas.java
@@ -14,6 +14,6 @@ public class Despesas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic herennnn
+        // Teste
     }
 }
