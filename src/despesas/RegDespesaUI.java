2cda020e298ee1ed903a2e1b64711c6334ca61f6
TOSHIBA <TOSHIBA@Pedro>
2013-03-15 14:58:50 +0000

    classe REgDespesaUI

--- src/despesas/RegDespesaUI.java
+++ src/despesas/RegDespesaUI.java
@@ -4,10 +4,22 @@
  */
 package despesas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author TOSHIBA
  */
 public class RegDespesaUI {
     
+    public static void RegDespesaUI(){
+        RegDespesaUI nome;
+}
+    
+    public static void IntroduzDados(){
+        String valor, descricao;
+        
+        valor=JOptionPane.showInputDialog("Insira o valor da despesa:");
+        descricao=JOptionPane.showInputDialog("Insira a descrição da despesa:");
+    }
 }
